6bb370fd64cbb61f99c27a4b7e9e661da15b39ec
Archi <51773524+mariotani25@users.noreply.github.com>
2022-02-18 11:24:25 +0700

    Create arch.sh

---
+++ arch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+export SCRIPTHOME="$(basename -- $PWD)"
+
+git clone "https://aur.archlinux.org/yay.git"
+cd yay
+makepkg -si --noconfirm
+
+cd /home/mario/$SCRIPTHOME
+
+# yay -Sy exa \
+#         curl \
+#         konsave \
+#         zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting grml-zsh-config oh-my-zsh-git \
+#         reflector \
+#         thefuck \
+#         yt-dlp \
+#         doas \
+#         latte-dock
+
+# Apps Install
+yay -Sy --needed - < pkg-files/min-pkgs.txt
+
+# Shell Setup
+cp conf/shellrc/.zshrc-arch /home/mario/.zshrc
+chsh -s /bin/zsh
+
+sudo cp conf/doas/doas.conf /etc/doas.conf
+
+# Restore KDE Setup
+konsave -i conf/konsave/konsave.knsv
+konsave -a konsave
+
+latte-dock --enable-autostart --default-layout /home/mario/$SCRIPTHOME/conf/latte/laptop.layout.latte
+
+
