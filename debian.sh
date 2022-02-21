c911d3345a53103cc633750e1fed8af44cd9b9f8
Archi <51773524+mariotani25@users.noreply.github.com>
2022-02-21 11:47:07 +0700

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -41,8 +41,8 @@ chmod +x $HOME/.oh-my-zsh/oh-my-zsh.sh
 cp conf/shellrc/.zshrc-deb $HOME/.zshrc
 
 # Setup .config
-cp conf/easyeffects $HOME/.config
-cp conf/Kvantum $HOME/.config
+cp -r conf/easyeffects $HOME/.config
+cp -r conf/Kvantum $HOME/.config
 
 ### restore KDE setup
 konsave -i conf/konsave/laptop.knsv
@@ -53,7 +53,7 @@ konsave -a laptop
 wget https://dl3.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTYzMjk1Mjg1MywidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiJiMDY3YTc4YTUyZDVjMDY1MzE3ODQwZjk0NmMwNDE0YjU3ZDVjM2VlMDFkNDhlN2MwODQwZTk2NGZlZjVlZGRhZWYyMmMxMWE2YjMxMTYxNmQ3ZWUwMDdhNTA2NjQ2ZmZlMThmYWI5MzY5YmFiN2VmNTllOGZiYWJmZWY1M2Q4ZCIsInQiOjE2NDU0MTg2MzEsInN0ZnAiOiIyMTFhYTMzZTViZjNkNmZkOWFjNWM2YjdiZDMzMDM1NyIsInN0aXAiOiIxODAuMjUzLjg3LjkyIn0.-_UCS5nA-neANhRektgXqsmcaaRXspMGygTME4J_vw0/Nordic.tar.xz
 tar -xf Nordic.tar.xz.1
 sleep 1
-sudo cp Nordic /usr/share/sddm/themes/
+sudo cp -r Nordic /usr/share/sddm/themes/
 sudo ./conf/sddm.sh
 
 latte-dock --enable-autostart --default-layout $HOME/$SCRIPTHOME/conf/latte/laptop.layout.latte
