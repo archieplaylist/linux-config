866a5c805f109faaadfe662cd6dfdb07c35f63ad
Archi <51773524+mariotani25@users.noreply.github.com>
2022-02-23 19:03:31 +0700

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -25,12 +25,12 @@ sudo apt install -y zsh \
                     thefuck
 
 # Apps
-sudo apt-get -y install firefox \
-                        flatpak
+sudo apt-get -y install flatpak
 
 ### setup flatpak repo
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install -y easyeffects
+flatpak install -y  firefox
+                    # easyeffects
 
 python3 -m pip install -U konsave yt-dlp
 
@@ -45,13 +45,12 @@ cp -r conf/easyeffects $HOME/.config
 cp -r conf/Kvantum $HOME/.config
 
 ### restore KDE setup
-konsave -i conf/konsave/laptop.knsv
+python3 -m konsave -i conf/konsave/laptop.knsv
 sleep 1
-konsave -a laptop
+python3 -m konsave -a laptop
 
 #### Download Nordic Theme Debian
-wget https://dl3.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTYzMjk1Mjg1MywidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiJiMDY3YTc4YTUyZDVjMDY1MzE3ODQwZjk0NmMwNDE0YjU3ZDVjM2VlMDFkNDhlN2MwODQwZTk2NGZlZjVlZGRhZWYyMmMxMWE2YjMxMTYxNmQ3ZWUwMDdhNTA2NjQ2ZmZlMThmYWI5MzY5YmFiN2VmNTllOGZiYWJmZWY1M2Q4ZCIsInQiOjE2NDU0MTg2MzEsInN0ZnAiOiIyMTFhYTMzZTViZjNkNmZkOWFjNWM2YjdiZDMzMDM1NyIsInN0aXAiOiIxODAuMjUzLjg3LjkyIn0.-_UCS5nA-neANhRektgXqsmcaaRXspMGygTME4J_vw0/Nordic.tar.xz
-tar -xf Nordic.tar.xz.1
+tar -xf conf/Nordic.tar.xz.1
 sleep 1
 sudo cp -r Nordic /usr/share/sddm/themes/
 sudo ./conf/sddm.sh
